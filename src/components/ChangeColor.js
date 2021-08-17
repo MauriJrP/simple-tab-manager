327849c0f9054fc145026c1ec61939be00f3e745
Mauricio Rodriguez <maurijrp2001@gmail.com>
2021-08-17 12:52:04 -0500

    Change color palette working, need to update to the best styles

--- src/components/ChangeColor.js
+++ src/components/ChangeColor.js
@@ -1,14 +1,35 @@
-function ChangeColor({ onClick }) {
+/*global chrome*/
+
+function ChangeColor({ showPalette, palette, setColorPalette }) {
 	return (
 		<div
-			className="img-container tabs-card__img-container"
+			className="img-container tabs-card__img-container tabs-card__color-palette"
 			title="Change color"
-			onClick={onClick}
+			onClick={showPalette}
 		>
 			<img
 				src={process.env.PUBLIC_URL + '/icons/color-palette.png'}
 				className="tabs-card__img"
 			/>
+			{palette && (
+				<div className="color-palette">
+					<div
+						className="color-palette__div color-palette__basic"
+						id="basic"
+						onClick={setColorPalette}
+					></div>
+					<div
+						className="color-palette__div color-palette__pink"
+						id="pink"
+						onClick={setColorPalette}
+					></div>
+					<div
+						className="color-palette__div color-palette__yellow"
+						id="yellow"
+						onClick={setColorPalette}
+					></div>
+				</div>
+			)}
 		</div>
 	);
 }
--- src/components/TabsCard.js
+++ src/components/TabsCard.js
@@ -10,7 +10,7 @@ function TabsCard({ openTabs }) {
 	let [minimized, setMinimized] = useState(false);
 	let [palette, setPalette] = useState(false);
 
-	const minimizeGroup = (element) => {
+	const minimizeGroup = (/*element*/) => {
 		setMinimized(!minimized);
 		chrome.storage.local.get('openTabs', ({ openTabs }) => {
 			openTabs.minimized = !openTabs.minimized;
@@ -18,9 +18,19 @@ function TabsCard({ openTabs }) {
 		});
 	};
 
-	const changePalette = () => {
+	const showPalette = () => {
 		setPalette(!palette);
 		console.log(palette);
+	};
+
+	const setColorPalette = (element) => {
+		const color = element.target.id;
+		chrome.storage.local.get('openTabs', ({ openTabs }) => {
+			openTabs.color = color;
+			chrome.storage.local.set({ openTabs: openTabs });
+		});
+		// console.log(color);
+		// console.log
 	};
 
 	return (
@@ -29,7 +39,11 @@ function TabsCard({ openTabs }) {
 				<h2 className={'tabs-card__title text-' + openTabs.color}>
 					{openTabs.nameGroup}
 				</h2>
-				<ChangeColor onClick={() => changePalette()} />
+				<ChangeColor
+					showPalette={showPalette}
+					palette={palette}
+					setColorPalette={setColorPalette}
+				/>
 				<div
 					className="img-container tabs-card__img-container"
 					title="Minimize"
